43b0982cd934be62774e1c2fe45565993122ddc4
asteiny <42418891+asteiny@users.noreply.github.com>
2018-08-15 12:02:39 -0700

    project map
    project map

---
+++ data/Projects_0.js
@@ -0,0 +1,25 @@
+var json_Projects_0 = {
+"type": "FeatureCollection",
+"name": "Projects_0",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "ProjectName": "Bend Zoning and Policy Incentives", "Location": "Bend, OR", "ProjectType": "Zoning & Policy Analysis" }, "geometry": { "type": "Point", "coordinates": [ -121.311789181856071, 44.05145834304335 ] } },
+{ "type": "Feature", "properties": { "ProjectName": "Walnut Park Feasibility Analysis", "Location": "Portland, OR", "ProjectType": "Real Estate & Development" }, "geometry": { "type": "Point", "coordinates": [ -122.662575951672721, 45.56074429977869 ] } },
+{ "type": "Feature", "properties": { "ProjectName": "Milwaukie Cottage Cluster Analysis", "Location": "Milwaukie, OR", "ProjectType": "Strategic Planning & Urban Design" }, "geometry": { "type": "Point", "coordinates": [ -122.643246782536309, 45.447346507511703 ] } },
+{ "type": "Feature", "properties": { "ProjectName": "Canby Development Offering", "Location": "Canby, OR", "ProjectType": "P3s & Development Offerings" }, "geometry": { "type": "Point", "coordinates": [ -122.690200555896951, 45.26355832430626 ] } },
+{ "type": "Feature", "properties": { "ProjectName": "PECI Asset Mapping", "Location": "Portland, OR", "ProjectType": "Urban Analytics" }, "geometry": { "type": "Point", "coordinates": [ -122.676670137501489, 45.524502107647876 ] } },
+{ "type": "Feature", "properties": { "ProjectName": "Fiscal Impacts of Land Use", "Location": "Sacramento, CA", "ProjectType": "Urban Analytics" }, "geometry": { "type": "Point", "coordinates": [ -121.485107398446345, 38.577034952586992 ] } },
+{ "type": "Feature", "properties": { "ProjectName": "Conservation and VMT Analysis", "Location": "Merced, CA", "ProjectType": "Urban Analytics" }, "geometry": { "type": "Point", "coordinates": [ -120.477413380801224, 37.309041457238152 ] } },
+{ "type": "Feature", "properties": { "ProjectName": "Envision Tomorrow Online Development", "Location": "Sacramento, CA", "ProjectType": "Urban Analytics" }, "geometry": { "type": "Point", "coordinates": [ -121.474798508240283, 38.584766620241574 ] } },
+{ "type": "Feature", "properties": { "ProjectName": "SB743 Case Studies", "Location": "California (Statewide)", "ProjectType": "Urban Analytics" }, "geometry": { "type": "Point", "coordinates": [ -121.468999757499418, 38.572524813121838 ] } },
+{ "type": "Feature", "properties": { "ProjectName": "Austin CodeNEXT", "Location": "Austin, TX", "ProjectType": "Urban Analytics" }, "geometry": { "type": "Point", "coordinates": [ -97.75468644966908, 30.257116250635889 ] } },
+{ "type": "Feature", "properties": { "ProjectName": "Gunnison Downtown Development Strategy", "Location": "Gunnison, CO", "ProjectType": "Zoning & Policy Analysis" }, "geometry": { "type": "Point", "coordinates": [ -106.928954427449767, 38.545463976330865 ] } },
+{ "type": "Feature", "properties": { "ProjectName": "Gunnison Rising Master Plan", "Location": "Gunnison, CO", "ProjectType": "Real Estate & Development" }, "geometry": { "type": "Point", "coordinates": [ -106.907933956013935, 38.546108281968742 ] } },
+{ "type": "Feature", "properties": { "ProjectName": "Crested Butte South Commercial Area Master Plan", "Location": "Gunnison County, CO", "ProjectType": "Strategic Planning & Urban Design" }, "geometry": { "type": "Point", "coordinates": [ -106.888765863287006, 38.813656198098634 ] } },
+{ "type": "Feature", "properties": { "ProjectName": "East Sherman Master Plan", "Location": "Coeur d'Alene, ID", "ProjectType": "Zoning & Policy Analysis" }, "geometry": { "type": "Point", "coordinates": [ -116.763071916624895, 47.67285871895578 ] } },
+{ "type": "Feature", "properties": { "ProjectName": "Fleet Block Feasibility Study", "Location": "Salt Lake City, UT", "ProjectType": "Real Estate & Development" }, "geometry": { "type": "Point", "coordinates": [ -111.900175114721705, 40.762197523458859 ] } },
+{ "type": "Feature", "properties": { "ProjectName": "Clackamas County Land Bank Authority", "Location": "Clackamas County, OR", "ProjectType": "P3s & Development Offerings" }, "geometry": { "type": "Point", "coordinates": [ -122.601689068893535, 45.359720940760056 ] } },
+{ "type": "Feature", "properties": { "ProjectName": "Code and Development Incentives", "Location": "Taos, NM", "ProjectType": "Zoning & Policy Analysis" }, "geometry": { "type": "Point", "coordinates": [ -105.57381859457783, 36.407174640615025 ] } },
+{ "type": "Feature", "properties": { "ProjectName": "Salem Vision", "Location": "Salem, OR", "ProjectType": "Strategic Planning & Urban Design" }, "geometry": { "type": "Point", "coordinates": [ -123.03868936778612, 44.938828282814647 ] } }
+]
+}
